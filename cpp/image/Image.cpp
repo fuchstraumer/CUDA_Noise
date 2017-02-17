b53244f1417fc2c298fd3ca17c37fef13d90aa7a
Keegan <jonesk74@uw.edu>
2017-02-16 18:09:27 -0800

    Outlining elements of the Image class, along with adding lodepng to the external libraries

---
+++ cpp/image/Image.cpp
@@ -0,0 +1,52 @@
+#include "stdafx.h"
+// LodePNG is used to handle saving PNG images.
+#include "lodepng\lodepng.h"
+#include "Image.h"
+
+ImageWriter::ImageWriter(int _width, int _height) : width(_width), height(_height) {
+	// Setup destination for raw data.
+	rawData.resize(width * height);
+}
+
+void ImageWriter::WritePNG(const char * filename, int compression_level) const{
+	if (compression_level == 0) {
+		// Saves uncompressed image using "pixelData" to "filename"
+		lodepng::encode(filename, &pixelData[0], width, height);
+	}
+	else {
+		// TODO: Implement compression. Need to study parameters of LodePNG more. See: https://raw.githubusercontent.com/lvandeve/lodepng/master/examples/example_optimize_png.cpp
+	}
+}
+
+void ImageWriter::ConvertRawData(){
+	// raw data has a size that is found by taking width * height in the constructor.
+	// Pixel data is four times this: each single location has four seperate entries,
+	// one entry per color channel per pixel. So, that's 4 * (size of raw data).
+	pixelData.resize(rawData.size() * 4);
+	// ^ note: using resize means this can be indexed like a normal C-style array.
+
+	/*
+	
+		Indexing into pixel data:
+
+		Getting the position of a pixel at coordinates (x,y) is done like so:
+		[4 * height * y + 4 * x] = index into 1D vector to given a 2D position.
+		
+		The position retrieved immediately above is the position of the first
+		color channel, however. So, this only gets us the red channel. Getting
+		all of the channels could be done like this:
+
+		1. Given Point (x,y), get current "offset" into the vector as:
+			size_t idx = [4 * height * y + 4 * x]
+		2. Now get color channel indices like so:
+			size_t r, g, b, a;
+			r = pixelData[idx + 0] = (set value of red channel)
+			g = pixelData[idx + 1] = (set value of green channel)
+			b = pixelData[idx + 2] = (set value of blue channel)
+			a = pixelData[idx + 3] = (set value of alpha channel)
+	*/
+}
+
+void ImageWriter::WriteBMP_Header(std::ofstream & output_stream) const{
+	// TODO: Implement this. Need to do some more checks on insuring 4-byte alignment and correct sizing, somehow.
+}
--- cpp/image/Image.h
+++ cpp/image/Image.h
@@ -3,6 +3,49 @@
 #define IMAGE_H
 #include "stdafx.h"
 
+/*
 
+	Class - ImageWriter
+
+	Writes an image to a file given data. Can write completely uncompressed
+	BMP images, mostly uncompressed PNG images, or highly compressed PNG images.
+
+	Uses LodePNG for PNG stuff. BMP stuff is written into this class.
+
+*/
+
+class ImageWriter {
+public:
+
+	// Initializes image and allocates space in vector used to hold raw data.
+	ImageWriter(int width, int height);
+
+	// Writes data contained in this image to file with given name.
+	void WriteBMP(const char* filename) const;
+
+	// Writes data contained in this image to PNG. Compression level set by
+	// second parameter, but is optional. Defaults to uncompressed.
+	void WritePNG(const char* filename, int compression_level = 0) const;
+
+	// Attaches a module to this object. Calls the module when WriteImage methods are called.
+	// TODO: Implement this.
+
+	// Converts raw image data in "rawData" to pixel data, with a bit depth of 32.
+	// This means that each pixel has four 8-byte (unsigned char) elements.
+	void ConvertRawData();
+
+private:
+	// Holds raw data grabbed from one of the noise modules.
+	std::vector<float> rawData;
+
+	// Holds pixel data converted from rawData.
+	std::vector<unsigned char> pixelData;
+
+	// Dimensions of this image
+	int width, height;
+
+	// Writes BMP header.
+	void WriteBMP_Header(std::ofstream& output_stream) const;
+};
 
 #endif // !IMAGE_H
