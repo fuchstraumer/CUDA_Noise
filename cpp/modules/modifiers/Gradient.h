3882e36b52a95c2f4488bb0db4fc9476c4fe3648
fuchstraumer <jonesk74@uw.edu>
2017-02-21 16:04:19 -0800

    Working on implementation of "curve" module, and "gradient" module.

--- cpp/modules/modifiers/Gradient.h
+++ cpp/modules/modifiers/Gradient.h
@@ -1,18 +1,28 @@
 #pragma once
 #ifndef GRADIENT_H
 #define GRADIENT_H
-#include "stdafx.h"
-#include "Base.h"
+#include "common/CommonInclude.h"
+#include "../Base.h"
 
 namespace noise {
 
 	namespace module {
 
-		class Gradient : public Module {
+		class Gradient2D : public Module {
 		public:
 
-			Gradient(int width, int height);
+			// Constructor defaults to assuming simple diagonal gradient.
+			Gradient2D(int width, int height, float x0 = 0.0f, float x1 = 1.0f, float y0 = 0.0f, float y1 = 1.0f);
 
+			// Set gradient using given values.
+			float SetGradient(float x0, float x1, float y0, float y1);
+
+			std::array<float, 4> GetGradient() const;
+
+		protected:
+
+			// Holds values defining the actual gradient.
+			std::array<float, 4> gradient;
 		};
 
 	}
